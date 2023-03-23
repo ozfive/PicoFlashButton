d444be1de14e957afee6c6ca57f1022f45c73441
Chris <straight.chris@gmail.com>
2023-03-23 15:18:17 -0700

    Somehow main.c got wiped before the last commit. Not sure why but here is the actual main.c file.

---
+++ C-Code/main.c
@@ -0,0 +1,25 @@
+#include "pico.h"
+#include "pico/stdlib.h"
+
+extern void _start();
+extern void button_isr();
+extern void alarm_isr();
+
+void alarm_init();
+void button_init();
+
+int main()
+{
+    stdio_init_all();
+    gpio_init(0);
+
+    irq_set_exclusive_handler(SYSTICK_IRQ, alarm_isr);
+    irq_set_enabled(SYSTICK_IRQ, true);
+
+    irq_set_exclusive_handler(GPIO_IRQ0, button_isr);
+    irq_set_enabled(GPIO_IRQ0, true);
+
+    _start();
+
+    return 0;
+}
